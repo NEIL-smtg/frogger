fdb7fb5c8fdfe9798a7487f45f29ecf1ecd8053d
NEIL-smtg <72685169+NEIL-smtg@users.noreply.github.com>
2020-12-08 00:40:47 +0800

    rewrite the code to print the moving objects

--- FROGGG/src/p4_group_8_repo/GameView.java
+++ FROGGG/src/p4_group_8_repo/GameView.java
@@ -20,7 +20,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class GameView extends ImageView{
+public class GameView{
 	private Scene gameScene;
 	private Stage gameStage;
 	AnimationTimer timer;
@@ -31,7 +31,7 @@ public class GameView extends ImageView{
 	private static final int GAME_HEIGHT = 800;
 	private Stage menuStage;
 	private int level;
-		
+	private int orilevel;
 	
 	public GameView(int level) {
 		SpeedDecider(level);
@@ -44,7 +44,7 @@ public class GameView extends ImageView{
 
 	private void showlives() {
 		BackgroundImage img[] = new BackgroundImage[3];
-		int lives = animal.showlives();
+		int lives = frog.showlives();
 		System.out.println(lives);
 		int gap=0;
 		
@@ -82,16 +82,6 @@ public class GameView extends ImageView{
 		}
 	}
 
-	private void SpeedDecider(int level) {
-		if (level==1) {
-			this.level=level;
-		}
-		else {
-			this.level=1+level/4;
-		}
-		
-	}
-
 	private void exitGame() {
 		MenuButton exit = new MenuButton("EXIT");
 		exit.setLayoutX(450);
@@ -114,9 +104,10 @@ public class GameView extends ImageView{
 					
 					if (option.get()==ButtonType.OK) {
 						background.stop();
-						background.stopMusic();
+						//background.stopMusic();
 						Menu.mainStage.show();
 						gameStage.close();
+					
 					}
 			}
 			
@@ -124,7 +115,17 @@ public class GameView extends ImageView{
 		
 		background.getChildren().add(exit);
 	}
-	
+
+	private void SpeedDecider(int level) {
+		orilevel=level;
+		if (level==1) {
+			this.level=level;
+		}
+		else {
+			this.level=(int) (1+level/3.5);
+		}
+		
+	}
 
 	public void createNewGame(Stage menuStage) {
 		this.menuStage=menuStage;
@@ -160,135 +161,78 @@ public class GameView extends ImageView{
 		gameStage.setScene(gameScene);
 		backgroundsetup();
 		
-		frog = new Animal("file:src/p4_group_8_repo/frog/froggerUp.png");
-		background.add(frog);
-		
-		ObstacleSetup o = new ObstacleSetup(level);
-		Obstacle[] obs=o.getarr();
-		
-		for (int i = 0; i < 3 ; i++) {
-			background.add(obs[i]);
-		}
-		//background.add(o.getarr()[0][0]);
-		background.start();
-	}/*
-	
-	private void game(){
-		gameScene=new Scene(background,GAME_WIDTH,GAME_HEIGHT);
-		gameStage = new Stage();
-		gameStage.setScene(gameScene);
-		
-		BackgroundImage purpleborder1 = new BackgroundImage("p4_group_8_repo/purple border.jpg");
-		BackgroundImage purpleborder2 = new BackgroundImage("p4_group_8_repo/purple border.jpg");
-		BackgroundImage blackborder = new BackgroundImage("p4_group_8_repo/black border.jpg");
-		BackgroundImage darkblue = new BackgroundImage("p4_group_8_repo/deepblue.jpg");
-		darkblue.setLayoutX(0);
-		darkblue.setLayoutY(0);
-		purpleborder1.setLayoutX(0);
-		purpleborder1.setLayoutY(435);
-		purpleborder2.setLayoutX(0);
-		purpleborder2.setLayoutY(700);
-		blackborder.setLayoutX(0);
-		blackborder.setLayoutY(470);
-		
-		background.add(darkblue);
-		background.add(blackborder);
-		background.add(purpleborder1);
-		background.add(purpleborder2);
-		
-	
-		//Obstacle obstacle = new Obstacle("file:src/p4_group_8_repo/truck1Right.png", 25, 25, 3);
-		//Obstacle obstacle1 = new Obstacle("file:src/p4_group_8_repo/truck2Right.png", 100, 100,2 );
-		//Obstacle obstacle2 = new Obstacle("file:src/p4_group_8_repo/truck1Right.png",0,  150, 1);
+		MovingObjects o = new MovingObjects(level);
+		Obstacle[] carlane1=o.getlane1();
+		Obstacle[] carlane4=o.getlane4();
+		Obstacle[] trucklane2= o.getlane2();
+		Obstacle[] trucklane3= o.getlane3();
+		Turtle[] turtlelane6 = o.getlane6();
+		WetTurtle[] turtlelane9 = o.getlane9();
+		Log[] loglane7 = o.getlane7();
+		Log[] loglane8 = o.getlane8();
+		Log[] loglane10 = o.getlane10();
 		
 		BackgroundImage froggerback = new BackgroundImage("file:src/p4_group_8_repo/Screen Shot 2017-05-29 at 10.02.14 PM.png");
 		background.add(froggerback);
-		
-		
-		background.add(new Log("file:src/p4_group_8_repo/log/log3.png", 150, 0, 166, 0.75*level));
-		background.add(new Log("file:src/p4_group_8_repo/log/log3.png", 150, 220, 166, 0.75*level));
-		background.add(new Log("file:src/p4_group_8_repo/log/log3.png", 150, 440, 166, 0.75*level));
-		//background.add(new Log("file:src/p4_group_8_repo/log3.png", 150, 0, 166, 0.75));
-		background.add(new Log("file:src/p4_group_8_repo/log/logs.png", 300, 0, 276, -2*level));
-		background.add(new Log("file:src/p4_group_8_repo/log/logs.png", 300, 400, 276, -2*level));
-		//background.add(new Log("file:src/p4_group_8_repo/logs.png", 300, 800, 276, -2));
-		background.add(new Log("file:src/p4_group_8_repo//log/log3.png", 150, 50, 329, 0.75*level));
-		background.add(new Log("file:src/p4_group_8_repo/log/log3.png", 150, 270, 329, 0.75*level));
-		background.add(new Log("file:src/p4_group_8_repo/log/log3.png", 150, 490, 329, 0.75*level));
-		//background.add(new Log("file:src/p4_group_8_repo/log3.png", 150, 570, 329, 0.75));
-		
-		
-		
-		
-		
-		background.add(new Turtle(500, 376, -1*level, 130, 130));
-		background.add(new Turtle(300, 376, -1*level, 130, 130));
-		background.add(new WetTurtle(700, 376, -1*level, 130, 130));
-		background.add(new WetTurtle(600, 217, -1*level, 130, 130));
-		background.add(new WetTurtle(400, 217, -1*level, 130, 130));
-		background.add(new WetTurtle(200, 217, -1*level, 130, 130));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 200, 100, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 0, 100, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 100, 120, -1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 200, 120, -1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 100, 140, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 200, 140, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 100, 160, -1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 300, 160, -1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 100, 180, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 200, 180, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 100, 200, -1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 200, 200, -1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 100, 220, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 200, 220, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 400, 220, 1));
-		//End end2 = new End();
-		//End end3 = new End();
-		//End end4 = new End();
-		//End end5 = new End();
-		background.add(new End(13,96));
-		background.add(new End(141,96));
-		background.add(new End(141 + 141-13,96));
-		background.add(new End(141 + 141-13+141-13+1,96));
-		background.add(new End(141 + 141-13+141-13+141-13+3,96));
-		
-		
-		animal = new Animal("file:src/p4_group_8_repo/frog/froggerUp.png");
-		background.add(animal);
-		
-		
-		
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck/truck1"+"Right.png", 0, 649, 1*level, 120, 120));
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck/truck1"+"Right.png", 300, 649, 1*level, 120, 120));
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck/truck1"+"Right.png", 600, 649, 1*level, 120, 120));
-		//background.add(new Obstacle("file:src/p4_group_8_repo/truck1"+"Right.png", 720, 649, 1, 120, 120));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car/car1Left.png", 100, 597, -1*level, 50, 50));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car/car1Left.png", 250, 597, -1*level, 50, 50));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car/car1Left.png", 400, 597, -1*level, 50, 50));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car/car1Left.png", 550, 597, -1*level, 50, 50));
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck/truck2Right.png", 0, 540, 1*level, 200, 200));
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck/truck2Right.png", 500, 540, 1*level, 200, 200));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car/car1Left.png", 500, 490, -5*level, 50, 50));
 		background.add(new Digit(0, 30, 280, 25));
-		//background.add(obstacle);
-		//background.add(obstacle1);
-		//background.add(obstacle2);
-		background.start();
-		//primaryStage.setScene(scene);
-		//primaryStage.show();
-		start();
+		if (orilevel<=3) {
+			background.add(new End(141,86));
+			background.add(new End(398,86));
+		}
+		else if (orilevel >=4 && orilevel<=6) {
+			background.add(new End(111,86));
+			background.add(new End(269,86));
+			background.add(new End(420,86));
+		}
+		else if (orilevel>=7 && orilevel<=8) {
+			background.add(new End(90,86));
+			background.add(new End(218,86));
+			background.add(new End(346,86));
+			background.add(new End(475,86));
+		}
+		else {
+			background.add(new End(13,86));
+			background.add(new End(141,86));
+			background.add(new End(269,86));
+			background.add(new End(398,86));
+			background.add(new End(525,86));
+		}
+			
 		
-	}	*/
+		//loop by 3
+		for (int i = 0; i < 3 ; i++) {
+			background.add(carlane1[i]);		
+			background.add(loglane8[i]);
+			background.add(turtlelane9[i]);
+		}
+		
+		//loop by 2
+		for (int i = 0; i < 2; i++) {
+			background.add(carlane4[i]);
+			background.add(trucklane2[i]);
+			background.add(trucklane3[i]);
+			background.add(turtlelane6[i]);
+			background.add(loglane7[i]);
+			background.add(loglane10[i]);
+		}
+		
+		
+		frog = new Animal("file:src/p4_group_8_repo/frog/froggerUp.png");
+		background.add(frog);
+
+		background.start();
+		start();
+	}
 	
 
 	public void createTimer() {
 	timer = new AnimationTimer() {
 	    @Override
 	    public void handle(long now) {
-	    	if (animal.changeScore()) {
-	    		setNumber(animal.getPoints());
+	    	if (frog.changeScore()) {
+	    		setNumber(frog.getPoints());
 	    	}
-	    	if (animal.getStop()) {
+	    	if (frog.getStop()) {
 	    		System.out.print("STOPP:");
 	    		
 	    		background.stopMusic();
@@ -296,7 +240,7 @@ public class GameView extends ImageView{
 	    		background.stop();
 	    		Alert alert = new Alert(AlertType.INFORMATION);
 	    		alert.setTitle("You Have Won The Game!");
-	    		alert.setHeaderText("Your High Score: "+animal.getPoints()+"!");
+	    		alert.setHeaderText("Your High Score: "+frog.getPoints()+"!");
 	    		alert.setContentText("Highest Possible Score: 800");
 	    		alert.show();
 	    		
@@ -322,7 +266,7 @@ public class GameView extends ImageView{
 		     BufferedWriter bw = new BufferedWriter(fw);
 		     PrintWriter out = new PrintWriter(bw))
 			{
-				out.print(animal.getPoints()+"\n");
+				out.print(frog.getPoints()+"\n");
 			} catch (IOException e) {}
 			
 			try(FileWriter fw = new FileWriter("src/viewManager/NameDatabase.txt", true);
@@ -352,7 +296,7 @@ public class GameView extends ImageView{
 			  int k = n - d * 10;
 			  n = d;
 			  background.add(new Digit(k, 30, 280 - shift, 25));
-			  shift+=30;
+			  shift+=20;
 		}
 	}
 }
