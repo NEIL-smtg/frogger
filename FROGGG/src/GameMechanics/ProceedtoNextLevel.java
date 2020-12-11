76ad4a7ddb579ea25680911221f2ba331287fe25
NEIL-smtg <72685169+NEIL-smtg@users.noreply.github.com>
2020-12-12 01:47:43 +0800

    Added this class to store temporary points

---
+++ FROGGG/src/GameMechanics/ProceedtoNextLevel.java
@@ -0,0 +1,15 @@
+package GameMechanics;
+
+//this class is used to store points when player proceed to next level 
+public class ProceedtoNextLevel {
+	int points;
+	
+	public void setpoint(int points) {
+		this.points=points;
+	}
+	
+	public int getPoints() {
+		return this.points;
+	}
+	
+}
