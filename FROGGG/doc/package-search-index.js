ffe9a0ac6db611c34456b047ad84d030d4554e79
NEIL-smtg <72685169+NEIL-smtg@users.noreply.github.com>
2020-12-18 02:19:48 +0800

    Converted to maven, generated javadoc , relocate JUnit tests

---
+++ FROGGG/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"application"},{"l":"Database"},{"l":"GameAnimations"},{"l":"GameMechanics"},{"l":"JUnitTest"},{"l":"Main"},{"l":"Panel"},{"l":"resources"},{"l":"Scene"},{"l":"ScreenDesign"}]
